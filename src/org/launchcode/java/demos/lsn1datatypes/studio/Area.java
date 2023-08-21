8f0bebe4e50fa8d6d352d25093b8eb5896167cc4
g0-0py <brendan@bsherry.io>
2023-08-21 18:48:33 -0500

    formatting

--- src/org/launchcode/java/demos/lsn1datatypes/studio/Area.java
+++ src/org/launchcode/java/demos/lsn1datatypes/studio/Area.java
@@ -4,15 +4,11 @@ public class Area {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
         System.out.println("Enter a radius: ");
         double radius = input.nextDouble();
         input.close();
 
         double area = Circle.getArea(radius);
         System.out.println("Area of " + area + " with radius of " + radius);
-
-
-
     }
 }
