acf4e37c2760d347173ed90ccfa91a2386e7c8fc
g0-0py <brendan@bsherry.io>
2023-08-22 18:27:28 -0500

    comment out broken code

--- src/org/launchcode/java/demos/lsn2controlflowandcollections/exercises/hashmapPractice.java
+++ src/org/launchcode/java/demos/lsn2controlflowandcollections/exercises/hashmapPractice.java
@@ -4,32 +4,33 @@ import java.util.Map;
 import java.util.Scanner;
 public class hashmapPractice {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+//        Scanner input = new Scanner(System.in);
 
         // The provided solution won't build :|
-
-        do {
-            System.out.print("Student: ");
-            newStudent = input.nextLine();
-
-            if (!newStudent.equals("")) {
-                System.out.print("ID: ");
-                Integer newID = input.nextInt();
-                classRoster.put(newID, newStudent);
-
-                input.nextLine();
-            }
-        } while(!newStudent.equals(""));
-
-        input.close();
-
-        System.out.println("\nClass roster:");
-
-        for (Map.Entry<Integer, String> student : classRoster.entrySet()) {
-            System.out.println(student.getValue() + "'s ID: " + student.getKey());
-        }
-
-        System.out.println("Number of students in roster: " + classRoster.size());
+//
+//        do {
+//            System.out.print("Student: ");
+//            newStudent = input.nextLine();
+//
+//            if (!newStudent.equals("")) {
+//                System.out.print("ID: ");
+//                Integer newID = input.nextInt();
+//                classRoster.put(newID, newStudent);
+//
+//                input.nextLine();
+//            }
+//        } while(!newStudent.equals(""));
+//
+//        input.close();
+//
+//        System.out.println("\nClass roster:");
+//
+//        for (Map.Entry<Integer, String> student : classRoster.entrySet()) {
+//            System.out.println(student.getValue() + "'s ID: " + student.getKey());
+//        }
+//
+//        System.out.println("Number of students in roster: " + classRoster.size());
     }
 
 }
+
